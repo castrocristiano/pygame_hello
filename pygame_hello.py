3dc62314a0c374701d4fff6d486d84ae1c2bf27e
Cristiano Castro <cscastro87@gmail.com>
2020-04-28 22:17:20 -0300

    Criando uma superfice

--- pygame_hello.py
+++ pygame_hello.py
@@ -1,12 +1,17 @@
 import pygame
 
+
 def main():
     pygame.init()
     pygame.display.set_caption("Iniciando com Pygame")
     tela = pygame.display.set_mode([300, 300])
     relogio = pygame.time.Clock()
     sair = False
-    cor_branca = (255,255,255)
+    cor_branca = (255, 255, 255)
+    cor_azul = (108, 194, 236)
+    cor_verde = (152, 231, 114)
+    sup = pygame.Surface((200, 200))
+    sup.fill(cor_azul)
 
     while sair == False:
         for event in pygame.event.get():
@@ -14,7 +19,10 @@ def main():
                 sair = True
         relogio.tick(27)
         tela.fill(cor_branca)
+        tela.blit(sup, [50, 50])
+
         pygame.display.update()
     pygame.quit()
+
 
 main()
