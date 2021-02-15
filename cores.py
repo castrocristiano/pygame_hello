dbff9bde82c45f839ad0d3f59f9a1bfd830b02d5
Cristiano da Silva Castro <cscastro87@gmail.com>
2021-02-15 13:28:44 -0300

    Eventos dos botões do mouse

---
+++ cores.py
@@ -0,0 +1,8 @@
+
+
+# Variáveis que contém a definição de cores
+cor_branca = (255, 255, 255)
+cor_azul = (108, 194, 236)
+cor_verde = (54, 182, 112)
+cor_vermelha = (255, 0, 0)
+cor_rosa = (253, 227, 226)
